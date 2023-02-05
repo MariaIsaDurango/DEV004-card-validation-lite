a4f32c3c8a0f94f826b4d01b1d836b1634c6a061
Mariaisa <mdurango1130@gmail.com>
2023-02-05 11:57:43 -0500

    add first files

---
+++ src/app.js
@@ -0,0 +1,49 @@
+function validate () {
+    //valor del input
+     const crediCard = parseInt(document.getElementById("crediCard").value);
+ // identifica valor ingresado en input sea caracter numerico
+  if (isNaN (crediCard)) {
+     alert ("ingresar solo datos numericos");
+ } else {
+     alert("validando datos ingresados");
+     //pedir que entre a mi algoritmo
+     isValid (crediCard)
+   }
+ }
+   
+
+ // Algoritmo
+  
+ function isValid (crediCard) {
+    console.log(crediCard)
+     const tarjetaNmumero = (document.getElementById("crediCard").value)
+     // dividir numero en digotos 
+     
+
+     
+     
+ }  
+ 
+
+ //for (let i=0; i<9; i++) {
+   // console.log(i);
+   //} 
+       
+    // tip:bucle for
+    //intenta recorrer crediCard
+    // 367734136876842
+    // 3
+    // 6
+    // 7
+    // 7
+
+//if(nums[0]==9 || nums[nums.length-1] == 9)
+			//return true;
+		//else
+			//return false;
+	//}
+
+ 
+ 
+ 
+ 
